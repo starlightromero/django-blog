822a095f6091bfba2577458f76a98da010049dd4
Starlight Romero <starlightromero@gmail.com>
2020-09-19 23:54:58 -0700

    updated posts to use database

--- Django_Blog/blog/views.py
+++ Django_Blog/blog/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Starlight Romero',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'March 21, 2020'
-    },
-    {
-        'author': 'Sid Arcidiacono',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'March 31, 2020'
-    }
-]
+from .models import Post
 
 
 def home(request):
     """Return Blog Home."""
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
