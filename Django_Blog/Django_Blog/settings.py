0f9ce7d8a242c08cf51c424cbad5b3f2b43ff029
Starlight Romero <starlightromero@gmail.com>
2020-09-20 08:32:01 -0700

    uses dotenv to load secret key

--- Django_Blog/Django_Blog/settings.py
+++ Django_Blog/Django_Blog/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
+from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'n1#*h$$^h%-)&x1tns4&#5j(z^cn_%mszo4kgk*1_3^jg1bg3w'
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
