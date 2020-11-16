8a7e32dc3b6df0860ba7ae4fea5b5bfc37ec788a
Starlight Romero <starlightromero@gmail.com>
2020-11-15 16:32:55 -0800

    added urlpattern for statis media_url

--- Django_Blog/urls.py
+++ Django_Blog/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from users import views as users_views
 
 urlpatterns = [
@@ -34,3 +36,8 @@ urlpatterns = [
     ),
     path("", include("blog.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
