03a85f5a307c1eea1a4c579ad0c3469004e5f5f0
Starlight Romero <starlightromero@gmail.com>
2020-11-16 18:24:25 -0800

    added email settings

--- Django_Blog/settings.py
+++ Django_Blog/settings.py
@@ -130,3 +130,10 @@ LOGIN_URL = "login"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smpt.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv("EMAIL_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_PASSWORD")
